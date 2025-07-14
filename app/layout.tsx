96c5119cbb7942241fb95b45ab0fd44ed3c33139
qalalweh <abduu-30-04@hotmail.com>
2025-07-14 13:35:31 +0300

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'qalalweh-portfolio',
+  description: 'hi',
+  generator: 'me',
 }
 
 export default function RootLayout({
